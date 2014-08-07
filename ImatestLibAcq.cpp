151f04b13500398d89ab9d15a921d6d066777b84
tcumby <ty@imatest.com>
2014-08-07 15:14:36 -0600

    Fixed the issue where the contents of the log CEdit box were not being saved to file.

--- ImatestLibAcq.cpp
+++ ImatestLibAcq.cpp
@@ -121,6 +121,8 @@ bool ImatestLibAcq::CaptureFrame()
     {
         cout << "Run Error!" << endl;
         cerr << e.what() << endl;
+		mwException e2 = e;
+		e2.print_stack_trace();
 	}
 
 	//im_orig.GetData((mxUint64*)m_buf,(mwSize)im_orig.ElementSize());
