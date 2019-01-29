7dda644f05d00f1c0fa859ce6980f66a6d951e05
Theia\Ty <ty@imatest.com>
2019-01-28 17:12:24 -0700

    Added a class to house information retrieved by a call to the Acquisition library  list_devices() function

---
+++ AcquisitionDeviceInfo.cpp
@@ -0,0 +1,172 @@
+/****************************************************************************
+*	Operator Console - an extensible user interface for the Imatest IT 		*
+*	library																	*
+*	Copyright (C) 2013 Imatest LLC.											*
+*																			*
+*	This program is free software: you can redistribute it and/or modify	*
+*	it under the terms of the GNU General Public License as published by	*
+*	the Free Software Foundation, either version 3 of the License, or		*
+*	(at your option) any later version.										*
+*																			*
+*	This program is distributed in the hope that it will be useful,			*
+*	but WITHOUT ANY WARRANTY; without even the implied warranty of			*
+*	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the			*
+*	GNU General Public License for more details.							*
+*																			*
+*	You should have received a copy of the GNU General Public License		*
+*	along with this program.  If not, see <http://www.gnu.org/licenses/>. 	*
+****************************************************************************/
+
+#include "AcquisitionDeviceInfo.h"
+
+
+
+AcquisitionDeviceInfo::AcquisitionDeviceInfo()
+{
+}
+
+AcquisitionDeviceInfo::AcquisitionDeviceInfo(mwArray structObj)
+{
+	m_isInitialized = load(structObj);
+}
+
+
+AcquisitionDeviceInfo::~AcquisitionDeviceInfo()
+{
+}
+
+bool AcquisitionDeviceInfo::load(mwArray structObj)
+{
+	bool success = false;
+	assert(structObj.ClassID() == mxSTRUCT_CLASS);
+
+	if (structObj.ClassID() == mxSTRUCT_CLASS) {
+		m_deviceName = extractDeviceName(structObj);
+		m_deviceID = extractDeviceID(structObj);
+		m_defaultFormat = extractDefaultFormat(structObj);
+		m_supportedFormats = extractSupportedFormats(structObj);
+		m_deviceFileSupported = extractDeviceFileSupported(structObj);
+		m_adaptorName = extractAdaptorName(structObj);
+		
+		
+		success = true;
+	}
+	return success;
+}
+
+int AcquisitionDeviceInfo::extractDeviceID(mwArray structObj)
+{
+	int16_t deviceID = -1;
+	std::string fieldName = "DeviceID";
+
+	if (hasField(structObj, fieldName.c_str)) {
+		mwArray deviceIDField = structObj.Get(fieldName.c_str, 1, 1);
+
+		if (deviceIDField.NumberOfElements() >= 0) {
+			deviceIDField.GetData(&deviceID, 1);
+		}
+	}
+
+	return (int)deviceID;
+}
+
+CString AcquisitionDeviceInfo::extractDeviceName(mwArray structObj)
+{
+	return extractStringField(structObj, "DeviceName");
+}
+
+CString AcquisitionDeviceInfo::extractAdaptorName(mwArray structObj)
+{
+	return extractStringField(structObj, "Adaptor");
+}
+
+CString AcquisitionDeviceInfo::extractDefaultFormat(mwArray structObj)
+{
+	return extractStringField(structObj, "DefaultFormat");
+}
+
+std::vector<CString> AcquisitionDeviceInfo::extractSupportedFormats(mwArray structObj)
+{
+	std::vector<CString> formats;
+
+	std::string fieldName = "SupportedFormats";
+	if (hasField(structObj, fieldName.c_str())) {
+		mwArray field = structObj.Get(fieldName.c_str(), 1, 1);
+
+		int numFormats = field.NumberOfElements();
+		if (numFormats > 0 && field.ClassID() == mxCELL_CLASS) {
+			assert(field.Get(1, 1).ClassID() == mxCHAR_CLASS);
+
+			formats.resize(numFormats);
+			
+			mwString formatString;
+			for (int idx = 1; idx <= numFormats; ++idx) {
+				formatString = field.Get(1, idx).ToString();
+				formats[idx - 1] = CString((const char*)formatString);
+			}
+			
+		}
+
+	}
+
+	return formats;
+}
+
+bool AcquisitionDeviceInfo::extractDeviceFileSupported(mwArray structObj)
+{
+	assert(structObj.ClassID() == mxSTRUCT_CLASS);
+
+	bool isSupported = false;
+	std::string fieldName = "DeviceFileSupported";
+
+	if (hasField(structObj, fieldName.c_str())) {
+		mwArray field = structObj.Get(fieldName.c_str(), 1, 1);
+		assert(field.ClassID() == mxLOGICAL_CLASS);
+
+		if (field.ClassID() == mxLOGICAL_CLASS && field.NumberOfElements() > 0) {
+			field.GetLogicalData(&isSupported, 1);
+		}
+	}
+
+	return isSupported;
+}
+
+bool AcquisitionDeviceInfo::hasField(mwArray structObj, const char * str)
+{
+	bool result = false;
+
+	if (structObj.ClassID() == mxSTRUCT_CLASS) {
+		int numFields = structObj.NumberOfFields();
+
+		if (numFields > 0) {
+			std::vector<mwString> fields((size_t)numFields);
+			for (int idx = 1; idx <= numFields; ++idx) {
+				fields[idx - 1] = mwString(structObj.GetFieldName(idx));
+			}
+
+			std::vector<mwString>::iterator item = std::find(fields.begin(), fields.end(), mwString(str));
+
+			result = item != fields.end();
+		}
+	}
+	
+	return result;
+}
+
+CString AcquisitionDeviceInfo::extractStringField(mwArray structObj, const char * fieldName)
+{
+	assert(structObj.ClassID() == mxSTRUCT_CLASS);
+
+	mwString value = "";
+
+	if (hasField(structObj, fieldName)) {
+		mwArray field = structObj.Get(fieldName, 1, 1);
+		assert(field.ClassID() == mxCHAR_CLASS);
+
+		if (field.ClassID() == mxCHAR_CLASS) {
+			value = field.ToString();
+		}
+	}
+
+	return CString((const char*)value);
+}
---
+++ AcquisitionDeviceInfo.h
@@ -0,0 +1,64 @@
+/****************************************************************************
+*	Operator Console - an extensible user interface for the Imatest IT 		*
+*	library																	*
+*	Copyright (C) 2019 Imatest LLC.											*
+*																			*
+*	This program is free software: you can redistribute it and/or modify	*
+*	it under the terms of the GNU General Public License as published by	*
+*	the Free Software Foundation, either version 3 of the License, or		*
+*	(at your option) any later version.										*
+*																			*
+*	This program is distributed in the hope that it will be useful,			*
+*	but WITHOUT ANY WARRANTY; without even the implied warranty of			*
+*	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the			*
+*	GNU General Public License for more details.							*
+*																			*
+*	You should have received a copy of the GNU General Public License		*
+*	along with this program.  If not, see <http://www.gnu.org/licenses/>. 	*
+****************************************************************************/
+
+#pragma once
+
+#include "afxstr.h"
+#include "mclcppclass.h"
+
+#include <algorithm>
+#include <string>
+#include <vector>
+
+///
+/// This class houses the information describing a acquisition device as returned by a call to list_devices()
+///
+class AcquisitionDeviceInfo
+{
+public:
+	AcquisitionDeviceInfo();
+	AcquisitionDeviceInfo(mwArray structObj);
+	~AcquisitionDeviceInfo();
+
+public:
+	CString m_adaptorName; //!< The acquisition adaptor name (e.g. winvideo)
+	CString m_deviceName;  //!< The device name
+	int m_deviceID;        //!< The device ID to be used as the sourceID input for the acquire_image() call
+	CString m_defaultFormat; //!< The default video format for the device that is used if no other format is specified in the acquire_image() call
+	std::vector<CString> m_supportedFormats; //!< The supported video formats for the device
+	bool m_deviceFileSupported; //!< Whether or not the device supports a configuration file (not currently supported by the Operator Console)
+
+	bool isValid() const { return m_isInitialized; }
+
+private:
+	bool m_isInitialized;
+
+	bool load(mwArray structObj);
+	int extractDeviceID(mwArray structObj); //!< Returns the value of the "DeviceID" field of structObj
+	CString extractDeviceName(mwArray structObj); //!< Returns the value of the "DeviceName" field of structObj
+	CString extractAdaptorName(mwArray structObj); //!< Returns the value of the "Adaptor" field of structObj
+	CString extractDefaultFormat(mwArray structObj); //!< Returns the value of the "DefaultFormat" field of structObj
+	std::vector<CString> extractSupportedFormats(mwArray structObj); //!< Returns the values of the "SupportedFormats" field of structObj
+	bool extractDeviceFileSupported(mwArray structObj); //!< Returns the value of the "DeviceFileSupported" field of structObj
+
+	static bool hasField(mwArray structObj, const char* str); //!< A simple method for determining if a mwArray struct has a given field name
+	static CString extractStringField(mwArray structObj, const char* fieldName); //!< A method for extract the value of a struct field that only contains a single string
+
+};
+
